02a0ee9f8f7d699c5d6a1d972938a61dd5385c73
Thomas Petersohn <26284950+tpetersohn@users.noreply.github.com>
2024-06-21 16:24:26 +0200

    Changes to be committed: 	deleted:    index.html 	modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	new file:   views/index.ejs

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ import express from "express"
 
 let app = express()
 
+app.set("view engine","ejs")
+
 async function getRandomPic(x){
 
     return await Promise.all(Array.from({length:x}, () => fetch('https://picsum.photos/200/300'))).then((pr) => pr.map((pr) => pr.url))
 
 }
         
-app.get('/', async (req,res) => {
+app.get('/', (req,res) => {
     
-    const imageUrls = await getRandomPic(10);
-    res.json(imageUrls)
+    getRandomPic(10).then((imageUrls) => {
 
+                        const imageHtml = imageUrls.map((v) => `${v} alt="Image description">`);
+                        res.render("index", { imageHtml });
+    
+    })
+    
 })
    
 
